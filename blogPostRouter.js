d3987728547d9a405907785250c50d2bafa1f331
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-04-20 09:57:32 -0700

    commit 2: added notes and checked all backend request functions for errors

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -1,19 +1,33 @@
+//strict mode prevents undeclared variables
+//allows for better handling becuase it catches bad syntax as well
 'use strict';
 
+//'require' used to import third party libraries
+
+// this variable imports express
 const express = require('express');
+
+//new express router instance
 const router = express.Router();
 
+//middleware additions
 const bodyParser = require('body-parser');
+
+//
 const jsonParser = bodyParser.json();
 
+//imports BlogPosts model from different module
+//interface that captures current state of the application's current Blog Posts
 const {BlogPosts} = require('./models');
 
 // added blog posts with these params:title, content, author,
+// Pre-set data to look at when the server starts
 BlogPosts.create(
      'The Fox vs The Dog',
      'The quick brown fox jumps over the lazy dog.English-language pangram—a sentence that contains all of the letters of the alphabet. It is commonly used for touch-typing practice, testing typewriters and computer keyboards, displaying examples of fonts, and other applications involving text where the use of all letters in the alphabet is desired. Owing to its brevity and coherence, it has become widely known.',
      'Charlotte Frates'
 );
+
 BlogPosts.create(
      'ETAOIN SHRDLU',
      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!',
@@ -23,43 +37,48 @@ BlogPosts.create(
 
 // when the root of this router is called with GET, return
 // all current blog posts
+// endpoint diff but gets routed to /blog-post by server.js
 router.get('/', (req, res) => {
-  res.json(BlogPosts.get());
+     // API responds by serializing the data returned by BlogPosts
+     res.json(BlogPosts.get());
 });
 
-
-// when a new blog is posted, make sure it's
-// got required fields (title', 'content','author'). if not,
-// log an error and return a 400 status code. if okay,
+//POST reqeust with specified requirements
+// when a new blog is posted, make sure it has (title', 'content','author').
+// if not,log an error and return a 400 status code. if okay,
 // add new item to BlogPosts and return it with a 201.
 router.post('/', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['title', 'content','author'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
+    //error
     if (!(field in req.body)) {
       const message = `The \`${field}\` is missing in request body`
       console.error(message);
       return res.status(400).send(message);
     }
   }
+
   const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
   res.status(201).json(item);
 });
 
 
-// when DELETE request comes in with an id in path,
+// DELETE request that comes in with an ID in path,
+// once deleted a status code of 200 gets prompted
 router.delete('/:id', (req, res) => {
   BlogPosts.delete(req.params.id);
   console.log(`Deleted blog entry \`${req.params.ID}\``);
   res.status(204).end();
 });
 
-// when PUT request comes in with updated item, ensure has
-// required fields. also ensure that item id in url path, and
-// item id in updated item object match. if problems with any
-// of that, log error and send back status code 400. otherwise
-// call `BlogPosts.update` with updated item.
+// PUT request to change/update any added entries based on ID in path
+// when PUT request comes in with updated item, ensure has required fields.
+// also ensure that item id in url path, and
+// item id in updated item object match.
+// if problems with any of that, log error and send back status code 400.
+// otherwise call `BlogPosts.update` with updated item.
 router.put('/:id', jsonParser, (req, res) => {
   const requiredFields = ['title', 'content','author'];
   for (let i=0; i<requiredFields.length; i++) {
@@ -83,6 +102,7 @@ router.put('/:id', jsonParser, (req, res) => {
 
   console.log(`Updating blog entry \`${req.params.id}\``);
 
+  //updates model
   const updatedBlog = BlogPosts.update({
     id: req.params.id,
     author: req.body.author,
@@ -92,4 +112,5 @@ router.put('/:id', jsonParser, (req, res) => {
   res.status(204).json(updatedBlog);
 })
 
+//exports router instantce so that it can be imported in main file server.js
 module.exports = router;
--- server.js
+++ server.js
@@ -1,33 +1,41 @@
+//strict mode prevents undeclared variables
+//allows for better handling becuase it catches bad syntax as well
 'use strict';
 
+//'require' used to import third party libraries
+
+// this variable imports express
 const express = require('express');
+
+// this variable imports morgan
+//go-to HTTP logging middleware for Express apps.
+//With a few lines of setup code, we can use Morgan to get nicely formatted, standardized logs of all requests made to our server
 const morgan = require('morgan');
 
+//the constant 'app' calls for 'express()'
 const app = express();
 
-//
+//constant that imports modularized route
 const blogPostRouter = require('./blogPostRouter');
 
-
-// log the http layer
+// app logs the http layer
 app.use(morgan('common'));
 
 // loads static resources
 app.use(express.static('public'));
 
+// endpoint request that transfers files from a given path
+// res.send method just prints out whatever you put in parenthesis onto the screen
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-
-// when requests come into `/blog-post` or
-//we'll route them to the express
-// router instances we've imported. Remember,
-// these router instances act as modular, mini-express apps.
-// any request to /blog-post should be routed to the blogPostRouter
+// when requests come into `/blog-post` they get routed to blogPostRouter
+//use this to get access to predefined posts stored in postman localhost:3000/blog-post
 app.use('/blog-post', blogPostRouter);
 
 
+//app set to PORT 3000
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is listening on port ${process.env.PORT || 3000}`);
 });
