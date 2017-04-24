fe4c3ec8f9857e0af3012c75c35f4b15e10827df
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-04-23 21:19:06 -0700

    commit 7: small changes to spacing

--- public/js/client.js
+++ public/js/client.js
@@ -2,45 +2,45 @@
 var title, content, author;
 
 //variable that holds blog entries
-var data = { };
+var data = {};
 
 // API get reqeust
 var firstRequest = {
-                              "url": "http://localhost:3000/blog-post",
-                              "dataType": "json",
-                              "contentType":"application/json; charset=utf-8",
-                              "method": "GET",
-                         };
+     "url": "http://localhost:3000/blog-post",
+     "dataType": "json",
+     "contentType": "application/json; charset=utf-8",
+     "method": "GET",
+};
 
 
 //API POST request
-var newBlogEntry    = {
-                         "url": "http://localhost:3000/blog-post",
-                         "dataType": "json",
-                         "contentType":"application/json; charset=utf-8",
-                         "method": "POST",
-                         "data":JSON.stringify(data)
-                    };
+var newBlogEntry = {
+     "url": "http://localhost:3000/blog-post",
+     "dataType": "json",
+     "contentType": "application/json; charset=utf-8",
+     "method": "POST",
+     "data": JSON.stringify(data)
+};
 //Captures original API preset posts
-var preSetPosts = { };
+var preSetPosts = {};
 
 //Renders Posts into DOM
-function showPosts(){
-     $.each(preSetPosts, function (index, value) {
+function showPosts() {
+     $.each(preSetPosts, function(index, value) {
           console.log(value.title);
           console.log(value.content);
           console.log(value.author);
 
-          var blogTemplate = '<h1 class="blog-title">'
-                              + value.title
-                              + '</h1>'
-                              + '<p class="lead">by <a href="#">'
-                              + value.author
-                              + '</a></p>'
-                              + '<hr>'
-                              + '<p class="blog-content lead">'
-                              + value.content
-                              + '</p>';
+          var blogTemplate = '<h1 class="blog-title">' +
+               value.title +
+               '</h1>' +
+               '<p class="lead">by <a href="#">' +
+               value.author +
+               '</a></p>' +
+               '<hr>' +
+               '<p class="blog-content lead">' +
+               value.content +
+               '</p>';
 
           $('#blogposts').append(blogTemplate);;
 
@@ -50,7 +50,7 @@ function showPosts(){
 
 
 
-$('#submit').on('click', function(event){
+$('#submit').on('click', function(event) {
      event.preventDefault();
 
      // updates global variables based on user input
@@ -72,22 +72,22 @@ $('#submit').on('click', function(event){
 
      console.log(data);
 
-     $.ajax(newBlogEntry).done(function (response) {
+     $.ajax(newBlogEntry).done(function(response) {
           console.log(response);
      });
 
 });
 
-$('#home').on('click', function(event){
+$('#home').on('click', function(event) {
      event.preventDefault();
      $("#blogposts").empty();
 
-     $.ajax(firstRequest).done(function (response) {
+     $.ajax(firstRequest).done(function(response) {
           //updates preSetPosts with any new additional posts
           preSetPosts = eval(response);
-          console.log (preSetPosts);
+          console.log(preSetPosts);
           showPosts();
-          });
+     });
 
 
 });
@@ -95,21 +95,21 @@ $('#home').on('click', function(event){
 
 
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-     $.ajax(firstRequest).done(function (response) {
+     $.ajax(firstRequest).done(function(response) {
           //updates preSetPosts with any new additional posts
           preSetPosts = eval(response);
-          console.log (preSetPosts);
+          console.log(preSetPosts);
           showPosts();
-          });
+     });
 
-     $('#new').on('click',function (){
+     $('#new').on('click', function() {
           $('#blogposts').addClass('hidden');
           $('#entry').removeClass('hidden');
      });
 
-     $('#home').on('click',function (){
+     $('#home').on('click', function() {
           $('#blogposts').removeClass('hidden');
           $('#entry').addClass('hidden');
      });
