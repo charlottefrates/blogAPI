f35b6cafbcd99e1819a9440366892a7cdb354c08
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-04-26 21:43:18 -0700

    commit 16: few changes

--- public/js/client.js
+++ public/js/client.js
@@ -4,14 +4,16 @@ var title, content, author;
 //variable that holds blog entries
 var data = {};
 
-// API get reqeust
+//Captures original API preset posts
+var preSetPosts = {};
+
+// API GET reqeust
 var firstRequest = {
      "url": "http://localhost:3000/blog-post",
      "dataType": "json",
      "contentType": "application/json; charset=utf-8",
      "method": "GET",
 };
-
 
 //API POST request
 var newBlogEntry = {
@@ -21,10 +23,17 @@ var newBlogEntry = {
      "method": "POST",
      "data": JSON.stringify(data)
 };
-//Captures original API preset posts
-var preSetPosts = {};
 
-//Renders Posts into DOM
+//API DELETE request
+var deleteBlogEntry = {
+    "url": "http://localhost:3000/blog-post/:id",
+    "dataType": "json",
+    "contentType": "application/json; charset=utf-8",
+    "method": "DELETE",
+}
+
+
+//Renders Blog Posts into DOM
 function showPosts() {
      $.each(preSetPosts, function(index, value) {
           console.log(value.title);
@@ -48,11 +57,48 @@ function showPosts() {
 
 }
 
+//Renders Blog TITLES into DOM
+function showTitle() {
+     $.each(preSetPosts, function(index, value) {
+          console.log(value.title);
+
+          var blogTemplate = '<h1 class="blog-title">'
+               + value.title
+               +'</h1>'
+               + '<button id="delete"type="submit" class="btn btn-primary" style="margin-right:10px;">'
+               + 'Delete'
+               + '</button>'
+               + '<button id="edit"type="submit" class="btn btn-primary style="margin-right:10px;">'
+               + 'Edit'
+               + '</button>'
+               + '<hr>';
+
+          $('#listtitle').append(blogTemplate);;
+
+     });
+
+}
+
+// Selects ids to be DELETED from Blog entries
+function deletePost() {
+    console.log(preSetPosts);
+    //select entry in preSetPosts to be deleted
+    // TODO: var entrySelection = element.closest('h1').attr('index');
+    // TODO: console.log(entrySelection);
+
+    //Take selection and input id number into DELETE reqeust
+    // TODO: deleteBlogEntry.url = "http://localhost:3000/blog-post/" + ${id}
+
+    //update reqeust into JSON stringify
+    //TODO: deleteBlogEntry.url = JSON.stringify(); TODO
+
+}
 
 
+
+// POST new blog entry
 $('#submit').on('click', function(event) {
      event.preventDefault();
-
      // updates global variables based on user input
      title = $('#title').val();
      content = $('#content').val();
@@ -81,8 +127,11 @@ $('#submit').on('click', function(event) {
 
 });
 
+
+// GET current blog entries
 $('#home').on('click', function(event) {
      event.preventDefault();
+     $('#editposts').addClass('hidden');
      $("#blogposts").empty();
 
      $.ajax(firstRequest).done(function(response) {
@@ -95,9 +144,31 @@ $('#home').on('click', function(event) {
 
 });
 
+// GET titles of current blog entries
+$('#edit').on('click', function(event){
+     event.preventDefault();
 
+     $('#editposts').removeClass('hidden');
+     $('#blogposts').addClass('hidden');
+     $('#entry').addClass('hidden');
+     $("#listtitle").empty();
 
+     $.ajax(firstRequest).done(function(response) {
+          //updates preSetPosts with any new additional posts
+          preSetPosts = eval(response);
+          console.log(preSetPosts);
+          showTitle();
+     });
 
+});
+
+// DELETE blog entry
+$('#delete').on('click', function(){
+    deletePost();
+    //TODO:  $.ajax(deleteBlogEntry).done(function(response) {console.log(response);});
+});
+
+//document load
 $(document).ready(function() {
 
      $.ajax(firstRequest).done(function(response) {
@@ -110,12 +181,14 @@ $(document).ready(function() {
      $('#new').on('click', function() {
           $('#blogposts').addClass('hidden');
           $('#entry').removeClass('hidden');
+
      });
 
      $('#home').on('click', function() {
           $('#blogposts').removeClass('hidden');
           $('#entry').addClass('hidden');
      });
+
 
 
 });
